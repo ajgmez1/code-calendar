4cf4fe00da9e5937ffe910644f3bbbf454a6c53b
ajgmez1 <ajgmez1@gmail.com>
2022-05-17 12:22:30 -0700

    poc for leetcode calendar

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,46 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios';
-// import Cors from 'cors';
 
 type Data = {
-  name: string
-}
+  data: {}
+};
 
-// const cors = initMiddleware(
-//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-//   Cors({ methods: ['POST'] })
-// )
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // axios.post('https://leetcode.com/graphql/', {
-  axios.get('https://random-data-api.com/api/company/random_company', {
-    // headers: {
-    //   'Content-Type': 'application/json',
-    //   'Accept': 'application/json',
-    // },
-    // body: JSON.stringify(
-      // query: `\n    query userProfileCalendar($username: String!, $year: Int) {\n  matchedUser(username: $username) {\n    userCalendar(year: $year) {\n      activeYears\n      streak\n      totalActiveDays\n      dccBadges {\n        timestamp\n        badge {\n          name\n          icon\n        }\n      }\n      submissionCalendar\n    }\n  }\n}\n    ","variables":{"username":"ajgmez1"}}`
-  // }, {
-    headers: {
-        // 'Content-Type': 'application/json',
-        // 'Accept': 'application/json',
-        // 'Access-Control-Allow-Origin': '*'
-      },
+  await axios.post('https://leetcode.com/graphql/', {
+      query: `\n    query userProfileCalendar($username: String!, $year: Int) {\n  matchedUser(username: $username) {\n    userCalendar(year: $year) {\n      activeYears\n      streak\n      totalActiveDays\n      dccBadges {\n        timestamp\n        badge {\n          name\n          icon\n        }\n      }\n      submissionCalendar\n    }\n  }\n}\n    `,
+      variables: { username: "ajgmez1" }
   })
-    .then(data => {
-      // console.log('data returned:', data);
-      console.log('data returned:', data.headers);
-      res.status(200).json({ name: 'John Doe' });
+    .then((d) => {
+      res.status(d.status).json(d.data.data.matchedUser.userCalendar);
     })
     .catch((e) => {
-      console.error('errors: ', e.response.status, e.response.data);
-      console.error('headers: ', e);
-      res.status(e.response.status).json({ name: '' });
+      console.error('errors: ', e);
+      res.status(e.response.status).json({ error: e });
     })
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,18 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     axios.get('api/hello')
-      .then((d) => console.log(d));
+      .then((d) => {
+        const calendar = d.data;
+        const dates = JSON.parse(calendar.submissionCalendar);
+        console.log(calendar);
+        console.log(calendar.activeYears);
+        console.log(JSON.parse(calendar.submissionCalendar));
+
+        for (const k in dates) {
+          console.log(k, new Date(k*1000));
+        }
+        
+      });
   }, []);
 
   return (
