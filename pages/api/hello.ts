453a1537678543b8139e9711ee822fba4183959f
ajgmez1 <ajgmez1@gmail.com>
2022-05-26 14:46:26 -0700

    add github search sample

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,23 +2,60 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios';
 
-type Data = {
-  data: {}
-};
-
-export default async function handler(
+export default async (
   req: NextApiRequest,
   res: NextApiResponse
-) {
-  await axios.post('https://leetcode.com/graphql/', {
-      query: `\n    query userProfileCalendar($username: String!, $year: Int) {\n  matchedUser(username: $username) {\n    userCalendar(year: $year) {\n      activeYears\n      streak\n      totalActiveDays\n      dccBadges {\n        timestamp\n        badge {\n          name\n          icon\n        }\n      }\n      submissionCalendar\n    }\n  }\n}\n    `,
-      variables: { username: "ajgmez1" }
-  })
-    .then((d) => {
-      res.status(d.status).json(d.data.data.matchedUser.userCalendar);
+) => {
+  const promises = [];
+  
+  promises.push(axios.post('https://leetcode.com/graphql', {
+    query: `
+      query userProfileCalendar($username: String!, $year: Int) {
+        matchedUser(username: $username) {
+          userCalendar(year: $year) {
+            activeYears
+            streak
+            totalActiveDays
+            dccBadges {
+              timestamp
+              badge {
+                name
+                icon
+              }
+            }
+            submissionCalendar
+          }
+        }
+      }`,
+    variables: { username: "ajgmez1", year: 2022 }
+  }));
+
+  promises.push(axios.get('https://api.github.com/search/commits?q=' + encodeURIComponent('author:ajgmez1 committer-date:>2022-01-01 sort:committer-date') + '&per_page=100&page=1')); // committer-date:>2016-01-01 sort:committer-date-asc'))); // sort:committer-date-asc repo:ajgmez1/rda-grid'));
+
+  await Promise.all(promises)
+    .then(([d,g]) => {
+      const lc: { calendar: any } = { calendar: {} };
+      const gh: { calendar: any } = { calendar: {} };
+
+      const lcCalendarRaw = JSON.parse(d.data.data.matchedUser.userCalendar.submissionCalendar);
+
+      for (const c in lcCalendarRaw) {
+        const d = new Date(+c*1000);
+        lc.calendar[d.toISOString().substring(0,10)] = lcCalendarRaw[c];
+      }
+
+      for (const c of g.data.items) {
+        const date =  c.commit.author.date.substring(0,10);
+        gh.calendar[date] = gh.calendar[date] ? +gh.calendar[date]+1 : 1;
+      }
+
+      res.status(d.status).json({
+        lc: lc,
+        gh: gh
+      });
     })
     .catch((e) => {
       console.error('errors: ', e);
       res.status(e.response.status).json({ error: e });
     })
-}
+};
