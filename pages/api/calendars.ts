b52b665656e509f1c9de17d8b83981ac45f0cb48
ajgmez1 <ajgmez1@gmail.com>
2022-05-27 12:00:49 -0700

    move range logic to api

--- pages/api/calendars.ts
+++ pages/api/calendars.ts
@@ -31,7 +31,9 @@ export default async (
     variables: { username: "ajgmez1", year: 2022 }
   }));
 
-  promises.push(axios.get('https://api.github.com/search/commits?q=' + encodeURIComponent('author:ajgmez1 committer-date:>2022-01-01 sort:committer-date') + '&per_page=100&page=1')); // committer-date:>2016-01-01 sort:committer-date-asc'))); // sort:committer-date-asc repo:ajgmez1/rda-grid'));
+  promises.push(axios.get('https://api.github.com/search/commits?q=' + 
+    encodeURIComponent('author:ajgmez1 committer-date:>2022-01-01 sort:committer-date') + 
+    '&per_page=100&page=1')); // committer-date:>2016-01-01 sort:committer-date-asc'))); // sort:committer-date-asc repo:ajgmez1/rda-grid'));
 
   await Promise.all(promises)
     .then(([d,g]) => {
@@ -40,17 +42,31 @@ export default async (
 
       const lcCalendarRaw = JSON.parse(d.data.data.matchedUser.userCalendar.submissionCalendar);
 
-      let range = DateTime.now();
+      let last = DateTime.now();
+      let first = last.minus({years: 1});
       for (let i = 0; i <= 12; i++) {
-        const y = range.year;
-        const m = range.month;
+        const y = first.year;
+        const m = first.month;
+        const day1 = first.set({day:1});
+        const start = i === 0 ? first.day : 1;
+        const finish = i === 12 ? first.day : first.daysInMonth;
 
-        lc.calendar[y] = lc.calendar[y] ? lc.calendar[y] : {};
-        lc.calendar[y][m] = lc.calendar[y][m] ? lc.calendar[y][m] : {};
-        gh.calendar[y] = gh.calendar[y] ? gh.calendar[y] : {};
-        gh.calendar[y][m] = gh.calendar[y][m] ? gh.calendar[y][m] : {};
+        const dateInfo = {
+          year: first.year,
+          month: first.month,
+          startDayOfWeek: day1.weekday,
+          range: [start, finish]
+        }
 
-        range = range.minus({months:1});
+        lc.calendar[y+'-'+m] = lc.calendar[y+'-'+m] ? lc.calendar[y+'-'+m] : {};
+        gh.calendar[y+'-'+m] = gh.calendar[y+'-'+m] ? gh.calendar[y+'-'+m] : {};
+
+        lc.calendar[y+'-'+m].info = dateInfo;
+        gh.calendar[y+'-'+m].info = dateInfo;
+        lc.calendar[y+'-'+m].data = {};
+        gh.calendar[y+'-'+m].data = {};
+
+        first = first.plus({months:1});
       }
 
       for (const c in lcCalendarRaw) {
@@ -59,7 +75,7 @@ export default async (
         const m = d.month;
         const day = d.day;
 
-        lc.calendar[y][m][day] = lcCalendarRaw[c];
+        lc.calendar[y+'-'+m].data[day] = lcCalendarRaw[c];
       }
 
       for (const c of g.data.items) {
@@ -68,7 +84,7 @@ export default async (
         const m = d.month;
         const day = d.day;
 
-        gh.calendar[y][m][day] = gh.calendar[y][m][day] ? +gh.calendar[y][m][day]+1 : 1; 
+        gh.calendar[y+'-'+m].data[day] = gh.calendar[y+'-'+m].data[day] ? +gh.calendar[y+'-'+m].data[day]+1 : 1; 
       }
 
       res.status(d.status).json({
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,37 +1,17 @@
 import Month from "./Month";
-import { DateTime } from 'luxon';
 
 function Calendar({ data = {}, name }: { data: any, name?: string }) {
   const { calendar = {} } = data;
   
-  console.log(name, calendar);
-  
-  const Months = (() => {
-    const m = [];
-    let last = DateTime.now(); // change with params
-    let first = last.minus({years: 1});
-    if (!calendar[first.year]) return;
-
-    let day = first;
-    for (let i = 0; i <= 12; i++) {
-      const start = i === 0 ? day.day : 1;
-      const finish = i === 12 ? day.day : day.daysInMonth;
-      const startDay = day.set({day:1}).weekday%7;
-      
-      m.push(<Month key={i} v={day.month} 
-                startDay={startDay}
-                data={calendar[day.year][day.month]} 
-                range={[start, finish]} />);
-
-      day = day.plus({months:1});
-    }
-    return m;
-  })();
-
   return (
     <div style={{padding: '5px'}}> 
       {name} <br/>
-      {Months}
+      {Object.entries(calendar).map(([k, v]) => (
+        <Month key={k} v={v.info.month} 
+          startDay={v.info.startDayOfWeek}
+          data={v.data} 
+          range={v.info.range} />
+      ))}
     </div>
   );
 }
